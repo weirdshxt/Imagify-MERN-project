177065707f5851d928a6d16054b1765a415ddf2b
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-08 03:16:47 +0530

    add hero-section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center my-20">
+      <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500">
+        <p>Best text to image generator!</p>
+        <img src={assets.star_icon} alt="" />
+      </div>
+      <h1 className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center">
+        Turn text to <span className="text-blue-600">image</span>, in seconds.
+      </h1>
+
+      <p className="text-center max-w-xl mx-auto mt-5">
+        Unleash your creativity with AI. Turn your imagination into visual art
+        in seconds – just type, and watch the magic happen.
+      </p>
+
+      <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-3 rounded-full">
+        Generate Images
+        <img className="h-6" src={assets.star_group} alt="" />
+      </button>
+
+      <div className="flex flex-wrap justify-center mt-15 gap-3">
+        {Array(6)
+          .fill("")
+          .map((item, idx) => (
+            <img
+              className="rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10"
+              src={idx % 2 === 0 ? assets.sample_img_2 : assets.sample_img_1}
+              alt=""
+              key={idx}
+              width={70}
+            />
+          ))}
+      </div>
+      <p className="mt-2 text-neutral-600">Generated images from imagify</p>
+    </div>
+  );
+};
+
+export default Header;
