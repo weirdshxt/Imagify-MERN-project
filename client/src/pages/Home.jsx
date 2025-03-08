4aafa3a6ac6a256483a8ecd81a371c6eedae6bd1
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-08 12:53:33 +0530

    add body's description-section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
+import Description from '../components/Description'
 
 const Home = () => {
   return (
     <div>
         <Header />
         <Steps />
+        <Description />
     </div>
   )
 }
