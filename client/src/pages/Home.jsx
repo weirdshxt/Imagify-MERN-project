05c4e64c85c1e86fddfca43a803c69f4fae552bc
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-08 13:38:03 +0530

    add footer-gen-btn

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
 import Testimonials from '../components/Testimonials'
+import GenerateBtn from '../components/GenerateBtn'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <Steps />
         <Description />
         <Testimonials />
+        <GenerateBtn />
     </div>
   )
 }
