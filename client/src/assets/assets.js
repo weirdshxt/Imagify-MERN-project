650b0e72d603719e5a4f40d59426d79007e9de70
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-08 13:26:24 +0530

    add body's review-section

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -58,22 +58,22 @@ export const stepsData = [
 export const testimonialsData = [
     {
         image:profile_img_1,
-        name:'Donald Jackman',
+        name:'Furquan Ahmad',
         role:'Graphic Designer',
         stars:5,
         text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
     },
     {
         image:profile_img_2,
-        name:'Richard Nelson',
+        name:'Salique Afzal',
         role:'Content Creator',
         stars:5,
         text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
     },
     {
         image:profile_img_1,
-        name:'Donald Jackman',
-        role:' Graphic Designer',
+        name:'Kabir Khan',
+        role:'Software Developer',
         stars:5,
         text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
     },
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Header />
         <Steps />
         <Description />
+        <Testimonials />
     </div>
   )
 }
