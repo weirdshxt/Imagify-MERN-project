7a9b0af1369b26f9d4b0deb655b979e3140ed91f
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-08 12:25:31 +0530

    add body's step-section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
 
 const Home = () => {
   return (
     <div>
         <Header />
+        <Steps />
     </div>
   )
 }
